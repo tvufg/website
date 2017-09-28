b6ab381b3e3bbc1ca8f1009c6ed950b5b265e7af
matheuspiment <matheuspiment@hotmail.com>
2017-09-28 15:51:10 -0300

    Pequenas correções de código

--- get_grade.php
+++ get_grade.php
@@ -6,28 +6,15 @@ $objDb = new db();
 $link = $objDb->conecta_mysql();
 
 $data = $_POST['getDia'];
-
 $grade = array();
 
 $sql = " SELECT id FROM dia WHERE data =  $data ";
-
 $resultado = mysqli_query($link, $sql);
-
 $registro = mysqli_fetch_array($resultado, MYSQLI_ASSOC);
 
 $dia = $registro['id'];
-
-// var_dump($registro);
-
 $sql = " SELECT fk_programa FROM dia_programa WHERE fk_dia = $dia ORDER BY hora ";
-
 $resultado = mysqli_query($link, $sql);
-
-// $registro = mysqli_fetch_array($resultado, MYSQLI_ASSOC);
-//
-// var_dump($registro);
-
-$i = 1;
 
 if (!$resultado) {
   die();
@@ -36,22 +23,13 @@ if (!$resultado) {
 date_default_timezone_set('America/Sao_Paulo');
 
 while ($registro = mysqli_fetch_array($resultado, MYSQLI_ASSOC)) {
-
   $pg = $registro['fk_programa'];
-
   $sql = " SELECT * FROM programa WHERE id = $pg ";
-
   $re = mysqli_query($link, $sql);
-
   $reg = mysqli_fetch_array($re, MYSQLI_ASSOC);
-
   $programa = array('titulo' => $reg['titulo'], 'hora' => $reg['hora'], 'descricao' => $reg['descricao']);
   $pgJSON = json_encode($programa);
-
   array_push($grade, $pgJSON);
-
-  $i = $i + 1;
-
 }
 
 $gradeJSON = json_encode($grade);
