72cec01f67f5f87a3f7420ea564a98f964b1f24a
matheuspiment <matheuspiment@hotmail.com>
2017-09-28 16:19:01 -0300

    Add borda nos cards:hover

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
     <title>TV UFG</title>
 
     <!-- Bootstrap CSS -->
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="assets/css/bootstrap.css">
     <link rel="stylesheet" href="assets/css/slider-materialize.css"  media="screen,projection"/>
     <link rel="stylesheet" href="assets/css/font-awesome.css">
@@ -236,7 +235,6 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="assets/js/jquery-3.2.1.min.js"></script>
     <script src="assets/js/popper.min.js"></script>
-    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script> -->
     <script type="text/javascript" src="assets/js/materialize.js"></script>
     <script type="text/javascript" src="assets/js/bootstrap.js"></script>
     <!-- JS para testes -->
